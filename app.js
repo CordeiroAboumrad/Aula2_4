97000bcae5707e9259dd24c382d11cb911888c80
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-04 18:58:22 -0300

    Encapsula variáveis ambiente com dotenv

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { studentRouter } from './routes/studentRouter.js';
+import dotenv from 'dotenv';
+
+const app = express();
+
+// require('dotenv').config();
+const dotEnv = dotenv.config();
+
+process.env.USER_DB_PASSWORD = "007007007";
 
 // Conecta ao MongoDB pelo Mongoose
 (async () => {
     try{
         await mongoose.connect(
-            'mongodb+srv://jeanpcaboumrad:007007007@bootcamp.738aq.mongodb.net/grades?retryWrites=true&w=majority',
+            `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@bootcamp.738aq.mongodb.net/grades?retryWrites=true&w=majority`,
             {
             useNewUrlParser: true,
             useUnifiedTopology: true
@@ -20,13 +28,12 @@ import { studentRouter } from './routes/studentRouter.js';
     }
 })();
 
-const app = express();
 
 app.use(express.json());
 app.use(studentRouter);
 
 
-app.listen(3000, () => console.log("API Iniciada"));
+app.listen(process.env.PORT, () => console.log("API Iniciada"));
 
 
 
