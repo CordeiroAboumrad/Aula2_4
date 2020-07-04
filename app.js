3aa2e60f48f8bfcfd2952cf4ea214da1d4cd1321
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-04 17:18:46 -0300

    Meu Primeiro Versionamento

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { studentRouter } from './routes/studentRouter.js';
+
+// Conecta ao MongoDB pelo Mongoose
+(async () => {
+    try{
+        await mongoose.connect(
+            'mongodb+srv://jeanpcaboumrad:007007007@bootcamp.738aq.mongodb.net/grades?retryWrites=true&w=majority',
+            {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+            }
+        );
+        console.log('Conectado ao Mongoose.');
+    }catch(error){
+        console.log('Erro ao conectar no MongoDB.');
+        console.log(error);
+    }
+})();
+
+const app = express();
+
+app.use(express.json());
+app.use(studentRouter);
+
+
+app.listen(3000, () => console.log("API Iniciada"));
+
+
+
+
+
+//await Account.updateOne({ agencia, conta }, { $inc: { "balance": deposit } })
+
---
+++ models/student.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const studentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    subject: {
+        type: String,
+        require: true
+    },
+    type:{
+        type: String,
+        require: true
+    },
+    value:{
+        type: Number,
+        require:true,
+        validate(value){
+            if(value<0) throw new Error("Valor negativo para a nota não permitido.");
+        }
+    },
+    lastModified:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+// Terceiro parâmetro obriga o mongoose a colocar a palavra no singular
+const studentModel = mongoose.model('student', studentSchema, 'student');
+
+// Exportação para uso em outras classes
+export {studentModel};
